f33cda4f846c55c52d63c1ac71e89ae6ecfd3b6c
mufid1012 <mufidghibran1012@gmail.com>
2024-04-04 15:22:13 +0700

    perulangan perbandingan jika arr sama dengan item, step 3

--- Algoritma Search.cpp
+++ Algoritma Search.cpp
@@ -37,7 +37,10 @@ void LinearSearch() {
         cin >> item;
         ctr = 0;
         i = 0;      //step 2
-
+         while (i < n)       //step 3
+        {
+            ctr++;
+            if (arr[i] == item)
         
     }
 }
