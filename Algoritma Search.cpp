201ee67d62968869919f2d4e45d2f59cd1e38d91
mufid1012 <mufidghibran1012@gmail.com>
2024-04-04 15:24:16 +0700

    membuat step 5

--- Algoritma Search.cpp
+++ Algoritma Search.cpp
@@ -48,5 +48,13 @@ void LinearSearch() {
             }
             i++;       //step 4
         } 
-    }
+
+         if (i == n)     //step 5
+            cout << "\n" << item << " not found in the array\n";
+        cout << "\nNumber  of comparisons: " << ctr << endl;
+
+        cout << "\nContinue search (y/n): ";
+        cin >> ch;
+    } while ((ch == 'y') || (ch == 'y'));
+    
 }
